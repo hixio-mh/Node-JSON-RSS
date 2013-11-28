28e276c3c9ddc5d59ee35fab984cb242550cec67
DonaldDerek <donaldderek.haddad@gmail.com>
2013-11-28 11:02:33 +0100

    remove related links

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.get('/rss/en', function(req, res){
               $("<div class='article'>"+item.description+"</div>").appendTo("body");   
               $(".article").find('p').eq(-1).remove();   
               $(".article").find('table').eq(-1).remove();
+              $(".article").find('ul').eq(-1).remove();
               $(".article").find('a').attr('target', '_system');  
               
               
